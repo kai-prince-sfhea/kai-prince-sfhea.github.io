f0a08d62842488d0d3518561907ea7f03c812b17
Kai Prince <kai.prince@manchester.ac.uk>
2025-07-09 10:32:48 +0100

    Updated Schema Extension (0.1.1) to fix MathJax rendering

---
+++ _site/papers/MathsNotes/mathjax-config.js
@@ -0,0 +1,49 @@
+// MathJax configuration for the math notes
+// This configuration is used to load MathJax and set up the necessary macros and options
+
+fetch('./mathjax-macros.json')
+    .then(response => response.json())
+    .then(jsonArray =>
+        {
+            const macros = jsonArray;
+            console.log(macros);
+            window.MathJax =
+            {
+                startup:
+                {
+                    ready: () =>
+                    {
+                        console.log('MathJax is loaded!');
+                        MathJax.startup.defaultReady();
+                        console.log('MathJax is ready!');
+                        MathJax.startup.promise.then(() =>
+                        {
+                            console.log('MathJax typeset complete!');
+                        });
+                    }
+                },
+                menuOptions:
+                {
+                    annotationTypes:
+                    {
+                        TeX: ['TeX', 'LaTeX', 'application/x-tex'],
+                        ContentMathML: ['MathML-Content', 'application/mathml-content+xml'],
+                        OpenMath: ['OpenMath']
+                    }
+                },
+                loader: {load: ['[tex]/texhtml','[tex]/html']},
+                tex:
+                {
+                    allowTexHTML: true,
+                    packages: {'[+]': ['texhtml','html']},
+                    macros: macros
+                }
+            };
+
+            (function () {
+            var script = document.createElement('script');
+            script.src = 'https://cdn.jsdelivr.net/npm/mathjax@4.0.0-beta.7/tex-mml-chtml.js';
+            script.async = true;
+            document.head.appendChild(script);
+            })();
+        });
